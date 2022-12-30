b52cda1fc587ee617d04339b73111250aeaa3bb0
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2022-12-30 06:43:03 -0600

    fix: knobs addon causes render failure

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-postcss",
-    "@storybook/addon-knobs",
   ],
   framework: {
     name: "@storybook/nextjs",
